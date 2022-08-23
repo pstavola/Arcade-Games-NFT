95598a0beb0d3a126b555a0248480bfb1a55b53b
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-08-23 16:58:55 +0200

    trying to build with slither

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,14 +7,14 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    goerli: {
-      url: "https://goerli.infura.io/v3/1fc7c7c3701c4083b769e561ae251f9a",
-      accounts: [process.env.pk]
-    },
-    mainnet: {
-      url: "https://mainnet.infura.io/v3/1fc7c7c3701c4083b769e561ae251f9a",
-      accounts: [process.env.pk]
-    }
+    // goerli: {
+    //   url: "https://goerli.infura.io/v3/1fc7c7c3701c4083b769e561ae251f9a",
+    //   accounts: [process.env.pk]
+    // },
+    // mainnet: {
+    //   url: "https://mainnet.infura.io/v3/1fc7c7c3701c4083b769e561ae251f9a",
+    //   accounts: [process.env.pk]
+    // }
   },
   etherscan: {
     apiKey:  {
